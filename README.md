e45f176560e383eaf801e65a4c0f5fb4cac4b42e
LeslyN <lnombertoc@gmail.com>
2018-03-03 23:18:32 -0500

    Dando estilos, actualizando readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Box Random
+
+## Objetivo
+
+El trabajo realizado en esta ocasión tiene como objetivo que teniendo un contenedor y su texto estos cambien de estilos; el contenedor cambiará de color y el texto cambiará en su tipo de fuente y tamaño cada vez que la página sea recargada, este trabajo está implementado con JSX.
+
+# Anexos
+
+![example](assets/docs/example.png)
+
+
--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 (() => {
-  let colors = ['#a9f78c', '#7bf2ea', '#f4e216', '#f28293', '#f7f574', '#f7381b'];
+  let colors = ['#66ffb2', '#d7c462', '#bbbcfd', '#6eff7a', '#ff664c', '#ff66b3'];
   let fonts = ['Nanum Gothic', 'Signika Negative', 'Oswald', 'Dosis', 'Lobster', 'Quintessential'];
+  let sizes = ['18px', '16px', '20px', '24px', '22px', '26px'];
 
   const BoxRandom = () => (
-    
     <div style={{backgroundColor: colors[Math.floor(Math.random() * colors.length)], 
                 fontFamily: fonts[Math.floor(Math.random() * fonts.length)],
-                height: '50vh',
-                padding: '1em'}}>
-      <p style={{fontSize: '16px',
-                verticalAlign: 'middle'}}>Bienvenidos</p>
+                height: 'auto',
+                border: '2px solid #000',
+                borderRadius: '5px'
+                }}>
+      <p style={{fontSize: sizes[Math.floor(Math.random() * sizes.length)],
+                padding: '4em',
+                letterSpacing: '.1em'}}>Bienvenidos</p>
     </div>
   );
   
